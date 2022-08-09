707aaf3156a296e6118a76342dd430e2e59e8204
ilija <komperdel@gmail.com>
2022-08-09 18:41:08 +0300

    create

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = (props) => {
+	const [counter, setCounter] = useState(0);
+
+	function calcPlus() {
+		if (counter < 10) {
+			setCounter(counter + 1);
+		}
+	}
+
+	function calcMin() {
+		if (counter > -10) {
+			setCounter(counter - 1);
+		}
+	}
+
+	function rndCounter() {
+		setCounter(+(Math.random() * (10 - -10) + -10).toFixed(0));
+	}
+
+	function reset() {
+		setCounter(props.counter);
+	}
+
+	return (
+		<div className="app">
+			<div className="counter">{counter}</div>
+			<div className="controls">
+				<button onClick={calcPlus}>INC</button>
+				<button onClick={calcMin}>DEC</button>
+				<button onClick={rndCounter}>RND</button>
+				<button onClick={reset}>RESET</button>
+			</div>
+		</div>
+	);
+};
 
 export default App;
